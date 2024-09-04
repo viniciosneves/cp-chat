3eecc89dc23fd60d0251342546af43416ca02c8a
Vinicios Neves <vinicios.neves@mac>
2024-09-04 08:45:16 -0300

    adding markdown

--- src/components/ChatBubble/index.tsx
+++ src/components/ChatBubble/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import ReactMarkdown from 'react-markdown';
 
 interface ChatBubbleProps {
     message?: string;
@@ -16,13 +17,27 @@ const Bubble = styled.div<{ $isSender?: boolean }>`
   background-color: ${({ $isSender }) => ($isSender ? '#007bff' : '#f1f0f0')};
   align-self: ${({ $isSender }) => ($isSender ? 'flex-end' : 'flex-start')};
   box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+  
+  // Estilos adicionais para o conteúdo de markdown
+  p {
+    margin: 0;
+  }
+
+  a {
+    color: ${({ $isSender }) => ($isSender ? '#ffdd57' : '#007bff')};
+  }
 `;
 
 const ChatBubble: React.FC<ChatBubbleProps> = ({ message, isSender = false }) => {
     if (!message) {
-        return null
+        return null;
     }
-    return <Bubble $isSender={isSender}>{message}</Bubble>;
+
+    return (
+        <Bubble $isSender={isSender}>
+            <ReactMarkdown>{message}</ReactMarkdown>
+        </Bubble>
+    );
 };
 
 export default ChatBubble;
